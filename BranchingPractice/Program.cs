48e887210b9043ebf26d971af00448a2e52e3e4a
jonplumb89 <jonplumb89@outlook.com>
2021-08-17 19:18:47 -0400

    first push

--- BranchingPractice/Program.cs
+++ BranchingPractice/Program.cs
@@ -7,6 +7,10 @@ namespace BranchingPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int x = 5;
+            int y = 5;
+
+            Console.WriteLine(x + y); 
         }
     }
 }
