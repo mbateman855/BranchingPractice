58acece2c33cc5742cbae039327de04ed24f63ed
Morgan Bateman <mbateman855@gmail.com>
2021-08-17 19:01:20 -0400

    Add project files.

---
+++ BranchingPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BranchingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
