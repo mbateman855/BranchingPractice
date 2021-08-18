2f0073437994b428816a84b6652f336eb35e7f07
Morgan Bateman <mbateman855@gmail.com>
2021-08-17 20:21:32 -0400

    Added an interface and Butt class

--- BranchingPractice/Program.cs
+++ BranchingPractice/Program.cs
@@ -2,7 +2,7 @@
 
 namespace BranchingPractice
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
@@ -11,7 +11,8 @@ namespace BranchingPractice
             int x = 5;
             int y = 4;
 
-            Console.WriteLine(x + y); 
+            Console.WriteLine(x + y);
+
         }
     }
 }
